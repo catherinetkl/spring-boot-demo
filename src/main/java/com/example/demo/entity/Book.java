ab3fb85c4541dd715da6619105df7cc846f1c59f
catherinetkl <catherinetkll@gmail.com>
2024-01-04 03:57:30 +0800

    updated pom.xml file version, added docker-compose.yml, modified application.properties to include dockerized postgresql host and port mapping and credentials for database testing in development

--- src/main/java/com/example/demo/entity/Book.java
+++ src/main/java/com/example/demo/entity/Book.java
@@ -2,16 +2,16 @@ package com.example.demo.entity;
 
 import java.sql.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "book", schema = "public")
-public class Book {    
+public class Book {
 
     @Id
     @Column(name = "id", nullable=false)
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -6,6 +6,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.demo.controller.HelloController;
+
 @SpringBootTest
 class DemoApplicationTests {
 
